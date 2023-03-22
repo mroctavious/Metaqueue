9b5f7eeef2f7bd1b72b4f0b8d4e3ae39ad918d27
Eric Octavio Rodriguez Garcia <mroctavious88@gmail.com>
2023-03-22 02:37:25 -0600

    0.0.3 Added count methods and also the destroy method to remove the queue.

--- include/metaqueue.hpp
+++ include/metaqueue.hpp
@@ -28,13 +28,25 @@
 #include <iostream>
 #include <errno.h>
 
-#define METAQUEUE_DEFAULT_QUEUE_PERMISSION 0660 //> Default queue user permission, User and Group can Read(4) + Write(2).
-#define METAQUEUE_DEFAULT_MAX_MESSAGES 10       //> Maximum number of enqueued messages.
-#define METAQUEUE_DEFAULT_MAX_MESSAGE_SIZE 2048 //> Maximum size of a single message.
-#define METAQUEUE_DEFAULT_QUEUE_FLAGS 0         //> Extra Queue flags
+
+#ifndef METAQUEUE_DEFAULT_QUEUE_PERMISSION 
+    #define METAQUEUE_DEFAULT_QUEUE_PERMISSION 0660 //> Default queue user permission, User and Group can Read(4) + Write(2).
+#endif
+
+#ifndef METAQUEUE_DEFAULT_MAX_MESSAGES 
+    #define METAQUEUE_DEFAULT_MAX_MESSAGES 10       //> Maximum number of enqueued messages.
+#endif
+
+#ifndef METAQUEUE_DEFAULT_MAX_MESSAGE_SIZE 
+    #define METAQUEUE_DEFAULT_MAX_MESSAGE_SIZE 2048 //> Maximum size of a single message.
+#endif
+
+#ifndef METAQUEUE_DEFAULT_QUEUE_FLAGS 
+    #define METAQUEUE_DEFAULT_QUEUE_FLAGS 0         //> Extra Queue flags
+#endif
 
 #ifndef EOK
-#define EOK 0 //> No error!
+    #define EOK 0 //> No error!
 #endif
 
 /**
@@ -66,7 +78,7 @@ namespace QueueMetafunctions
      * @param errvalue error number identifier.
      * @return std::string Error description.
      */
-    std::string create_error(int errvalue)
+    inline std::string create_error(int errvalue)
     {
         char buffer[192] = {0};
         snprintf(buffer, sizeof(buffer), "The error generated was %d That means:%s.", errvalue, strerror(errvalue));
@@ -89,7 +101,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will create the data object depending if its a complex class or a simple class(Structure, Primitive types, Scalars).
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      * @tparam value_size size in bytes sizeof T.
      * @tparam can_be_memcpyed type attribute, can the object be used with memcpy.
@@ -100,7 +112,7 @@ namespace QueueMetafunctions
     {
         /**
          * @brief This method will execute the data_builder metafunction.
-         * 
+         *
          * @param buffer char* Buffer to store the data temporary.
          * @param nbytes int, number of bytes read from queue.
          * @return T Instantied data type with the content of the buffer.
@@ -113,7 +125,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will create the data to a simple class(Structure, Primitive types, Scalars).
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      * @tparam value_size size in bytes sizeof T.
      */
@@ -122,7 +134,7 @@ namespace QueueMetafunctions
     {
         /**
          * @brief This method will execute the data_builder metafunction.
-         * 
+         *
          * @param buffer char* Buffer to store the data temporary.
          * @param nbytes int, number of bytes read from queue.
          * @return T Instantied data type with the content of the buffer.
@@ -146,7 +158,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will create a complex data object, the class must have the constructor Obj(const char* data, size_t size) in order to work.
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      * @tparam value_size size in bytes sizeof T.
      */
@@ -155,7 +167,7 @@ namespace QueueMetafunctions
     {
         /**
          * @brief This method will execute the data_builder metafunction.
-         * 
+         *
          * @param buffer char* Buffer to store the data temporary.
          * @param nbytes int, number of bytes read from queue.
          * @return T Instantied data type with the content of the buffer.
@@ -254,7 +266,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will get the message from the queue and convert it to the datatype T, the method can be wait until a message arrives or wait for a set of seconds.
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      * @tparam value_size sizeof T
      * @tparam can_be_memcpyed type attribute, can the object be used with memcpy.
@@ -266,7 +278,7 @@ namespace QueueMetafunctions
 
         /**
          * @brief This method will wait until a new message can be read from the queue, the seconds given are the wait time, if no message arrives between the moment the method is executed (now) and(+) the timeout_seconds parameter, the function will return an empty instance of the object and a false value in the second return value pair.
-         * 
+         *
          * @param queue_fd Queue file descriptor.
          * @param buffer Pointer to the buffer where the data will be stored when reading the queue.
          * @param buffer_size sizeof the buffer.
@@ -279,7 +291,7 @@ namespace QueueMetafunctions
             auto tm = QueueMetafunctions::timeout(timeout_seconds);
             if ((nbytes = mq_timedreceive(queue_fd, buffer, buffer_size, NULL, &tm)) > 0)
             {
-                if (nbytes < 0)
+                if (nbytes <= 0)
                 {
                     throw std::runtime_error(create_error(errno));
                 }
@@ -290,7 +302,7 @@ namespace QueueMetafunctions
 
         /**
          * @brief This method will wait until a new message is received.
-         * 
+         *
          * @param queue_fd Queue file descriptor.
          * @param buffer Pointer to the buffer where the data will be stored when reading the queue.
          * @param buffer_size sizeof the buffer.
@@ -314,7 +326,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will remove a message from the queue.
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      * @tparam can_be_memcpyed type attribute, can the object be used with memcpy.
      * @tparam is_trivial type attribute, is the object a simple structure which can be used with memcpy too.
@@ -324,7 +336,7 @@ namespace QueueMetafunctions
     {
         /**
          * @brief Execute the pop metafunction.
-         * 
+         *
          * @param queue_fd Queue file descriptor.
          * @param buffer Pointer to the buffer where the data will be stored when reading the queue.
          * @param buffer_size sizeof the buffer.
@@ -340,7 +352,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will remove a message from the queue.
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      */
     template <typename T>
@@ -352,7 +364,7 @@ namespace QueueMetafunctions
 
         /**
          * @brief Execute the pop metafunction.
-         * 
+         *
          * @param queue_fd Queue file descriptor.
          * @param buffer Pointer to the buffer where the data will be stored when reading the queue.
          * @param buffer_size sizeof the buffer.
@@ -377,7 +389,7 @@ namespace QueueMetafunctions
 
     /**
      * @brief This metafunction will remove a message from the queue.
-     * 
+     *
      * @tparam T Datatype which the queue will be working with.
      */
     template <typename T>
@@ -389,7 +401,7 @@ namespace QueueMetafunctions
 
         /**
          * @brief Execute the pop metafunction.
-         * 
+         *
          * @param queue_fd Queue file descriptor.
          * @param buffer Pointer to the buffer where the data will be stored when reading the queue.
          * @param buffer_size sizeof in bytes of the buffer.
@@ -414,50 +426,47 @@ namespace QueueMetafunctions
 
 }; // namespace QueueMetafunctions
 
-
 /**
  * @brief This class will simplify the way you can send and receive messages from the OS Queue System, default driver is POSIX MQueue.
- * 
+ *
  * @tparam T Datatype which the queue will be working with.
  * @tparam QueuePermission Permission of the queue, default 0660, User,Group(Read+Write)
  * @tparam MaxMessages Max number of enqueued messages, default 10.
  * @tparam MaxMessageSize Max size of the message in bytes.
- * @tparam MessageBufferSize Max size of the buffer.
  * @tparam QueueFlags Extra Queue flags.
  */
 template <typename T = std::void_t<>,
           int QueuePermission = METAQUEUE_DEFAULT_QUEUE_PERMISSION,
           int MaxMessages = METAQUEUE_DEFAULT_MAX_MESSAGES,
           int MaxMessageSize = METAQUEUE_DEFAULT_MAX_MESSAGE_SIZE,
-          int MessageBufferSize = METAQUEUE_DEFAULT_MAX_MESSAGE_SIZE,
           int QueueFlags = METAQUEUE_DEFAULT_QUEUE_FLAGS>
 class metaqueue
 {
-    using type = metaqueue<T, QueuePermission, MaxMessages, MaxMessageSize, MessageBufferSize, QueueFlags>; //> Current meta type.
-    typedef typename QueueMetafunctions::get_datatype<T>::type value_type; //> Value type depending on the input.
-    typedef typename std::add_lvalue_reference<value_type>::type value_ref; //> Safe Reference type of the datatype given.
-    static const constexpr bool is_memcpyed = std::is_standard_layout<T>::value; //> Bool which indicates if the object can be memcpied.
-    static const constexpr bool is_trivial = std::is_trivial<T>::value; //> Check if the datatype is trivial(Simple structure).
+    using type = metaqueue<T, QueuePermission, MaxMessages, MaxMessageSize, QueueFlags>; //> Current meta type.
+    typedef typename QueueMetafunctions::get_datatype<T>::type value_type;               //> Value type depending on the input.
+    typedef typename std::add_lvalue_reference<value_type>::type value_ref;              //> Safe Reference type of the datatype given.
+    static const constexpr bool is_memcpyed = std::is_standard_layout<T>::value;         //> Bool which indicates if the object can be memcpied.
+    static const constexpr bool is_trivial = std::is_trivial<T>::value;                  //> Check if the datatype is trivial(Simple structure).
 
 private:
-    bool dequeued_message; //> Boolean which indicates if the message could be read from the queue.
-    mqd_t queue_fd; //> Queue file descriptor.
-    struct mq_attr attr; //> Attributes of the queue.
-    std::string mailbox_name; //> Name of the Queue.
-    char buffer[MessageBufferSize]; //> Raw buffer where the bytes will be stored while doing queue.
+    bool dequeued_message;       //> Boolean which indicates if the message could be read from the queue.
+    mqd_t queue_fd;              //> Queue file descriptor.
+    struct mq_attr attr;         //> Attributes of the queue.
+    std::string mailbox_name;    //> Name of the Queue.
+    char buffer[MaxMessageSize]; //> Raw buffer where the bytes will be stored while doing queue.
 
     /**
      * @brief This method will set the buffer to zeros.
-     * 
+     *
      */
     void clean_buffer()
     {
-        std::memset(buffer, 0, MessageBufferSize);
+        std::memset(buffer, 0, MaxMessageSize);
     }
 
     /**
      * @brief This method will crean all variables of the class.
-     * 
+     *
      */
     void clean()
     {
@@ -468,7 +477,7 @@ private:
 
     /**
      * @brief Set the name object
-     * 
+     *
      * @param _mailbox_name Name of the Queue.
      */
     void set_name(const char *_mailbox_name)
@@ -486,7 +495,7 @@ private:
 
     /**
      * @brief This method will open the queue and assing the file descriptor.
-     * 
+     *
      */
     void init()
     {
@@ -505,7 +514,7 @@ private:
 public:
     /**
      * @brief Construct a new metaqueue object
-     * 
+     *
      * @param queue_name Name of the queue.
      */
     metaqueue(std::string queue_name)
@@ -517,23 +526,30 @@ public:
 
     /**
      * @brief Destroy the metaqueue object.
-     * 
+     *
      */
-    ~metaqueue() {}
+    ~metaqueue()
+    {
+        if (mq_close(queue_fd) != 0)
+        {
+            std::cerr << QueueMetafunctions::create_error(errno) << std::endl;
+        }
+    }
 
     /**
      * @brief This method will return the status of the dequeue operation.
-     * 
+     *
      * @return true The message was succesfully dequeued and converted.
      * @return false A problem ocurred while reading from queue or timeout reached.
      */
-    bool was_dequeued(){
+    bool was_dequeued()
+    {
         return dequeued_message;
     }
 
     /**
      * @brief This method will try to enqueue a message to the queue.
-     * 
+     *
      * @param data Data reference which will be stored in the queue.
      * @param priority Priority of the message.
      */
@@ -541,6 +557,10 @@ public:
     {
         try
         {
+            if (mq_notify(queue_fd, NULL) == -1)
+            {
+                throw std::runtime_error(QueueMetafunctions::create_error(errno));
+            }
             return QueueMetafunctions::push<value_type, is_memcpyed, is_trivial>::run(queue_fd, data, priority);
         }
         catch (const std::exception &e)
@@ -551,7 +571,7 @@ public:
 
     /**
      * @brief This method will try to dequeue a message from the queue.
-     * 
+     *
      * @param timeout If timeout is set to -1(default) then the method will wait until a message arrives otherwise if the value is no negative, then it will wait maximum int timeout seconds and the method will return.
      * @param priority Priority of the message.
      * @return value_type Returns the datatype given in the template, the value is valid if and only if the method was_dequeued() returns true.
@@ -562,7 +582,7 @@ public:
         {
             clean_buffer();
             dequeued_message = false;
-            auto response = QueueMetafunctions::pop<value_type, is_memcpyed, is_trivial>::run(queue_fd, buffer, MessageBufferSize, timeout, &priority);
+            auto response = QueueMetafunctions::pop<value_type, is_memcpyed, is_trivial>::run(queue_fd, buffer, MaxMessageSize, timeout, &priority);
             dequeued_message = response.second;
             return response.first;
         }
@@ -575,7 +595,7 @@ public:
 
     /**
      * @brief This method will try to enqueue a message to the queue.
-     * 
+     *
      * @param data Data reference which will be stored in the queue.
      * @param priority Priority of the message.
      */
@@ -586,7 +606,7 @@ public:
 
     /**
      * @brief This method will try to dequeue a message from the queue.
-     * 
+     *
      * @param timeout If timeout is set to -1(default) then the method will wait until a message arrives otherwise if the value is no negative, then it will wait maximum int timeout seconds and the method will return.
      * @param priority Priority of the message.
      * @return value_type Returns the datatype given in the template, the value is valid if and only if the method was_dequeued() returns true.
@@ -598,7 +618,7 @@ public:
 
     /**
      * @brief This method will try to enqueue a message to the queue.
-     * 
+     *
      * @param data Data reference which will be stored in the queue.
      * @param priority Priority of the message.
      */
@@ -609,7 +629,7 @@ public:
 
     /**
      * @brief This method will try to dequeue a message from the queue.
-     * 
+     *
      * @param timeout If timeout is set to -1(default) then the method will wait until a message arrives otherwise if the value is no negative, then it will wait maximum int timeout seconds and the method will return.
      * @param priority Priority of the message.
      * @return value_type Returns the datatype given in the template, the value is valid if and only if the method was_dequeued() returns true.
@@ -617,5 +637,86 @@ public:
     value_type read(int timeout = -1, unsigned int priority = 0)
     {
         return pop(timeout, priority);
+    }
+
+    /**
+     * @brief This method will count how many messages are in the current queue.
+     * 
+     * @return long Number of messages in the current queue.
+     */
+    long count()
+    {
+        try
+        {
+            if (mq_getattr(queue_fd, &attr) == -1)
+            {
+                throw std::runtime_error(QueueMetafunctions::create_error(errno));
+            }
+            return attr.mq_curmsgs;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        return -1;
+    }
+
+    /**
+     * @brief This method will count how many messages are in the selected queue.
+     * 
+     * @param _queue_name Name of the queue to count.
+     * @return long Number of messages in the selected queue.
+     */
+    static long count(std::string _queue_name)
+    {
+        try
+        {
+            std::string queue_name = "/" + _queue_name;
+            mqd_t queue_fd;
+            struct mq_attr attr;
+            if ((queue_fd = mq_open(queue_name.c_str(), O_RDONLY)) == -1)
+            {
+                perror("Server: mq_open (server)");
+                throw std::runtime_error("Error while trying to open the queue:" + queue_name + "\n" + QueueMetafunctions::create_error(errno));
+            }
+
+            if (mq_getattr(queue_fd, &attr) == -1)
+            {
+                if (mq_close(queue_fd) != 0)
+                {
+                    std::cerr << QueueMetafunctions::create_error(errno) << std::endl;
+                }
+                throw std::runtime_error(QueueMetafunctions::create_error(errno));
+            }
+
+            if (mq_close(queue_fd) != 0)
+            {
+                std::cerr << QueueMetafunctions::create_error(errno) << std::endl;
+            }
+            return attr.mq_curmsgs;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        return -1;
+    }
+
+    /**
+     * This method will destroy the current queue. Carefull with this method, if the queue is destroyed the messages will too.
+    */
+    void unlink()
+    {
+        try
+        {
+            if (mq_unlink(mailbox_name.c_str()) != 0)
+            {
+                throw std::runtime_error(QueueMetafunctions::create_error(errno));
+            }
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
     }
 };
